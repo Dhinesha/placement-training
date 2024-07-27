f5aee0fed19ac40efa006257df909494580343c0
Dhinesha <147994787+Dhinesha@users.noreply.github.com>
2024-07-27 20:49:09 +0530

    Create Sum.c

---
+++ 27.07.2024/Sum.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int d1, d2, sum;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &d1, &d2);
+    sum = d1 + d2;
+    printf("Sum: %d\n", sum);
+    return 0;
+}
